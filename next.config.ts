ea9607fb1b61620a958df0c20a6d71336c34aadc
Anoop Karnik <danoopkarnik@gmail.com>
2025-08-08 23:50:05 +0530

    Body size limit increased

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   eslint:{
     ignoreDuringBuilds: true,
-  }
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
+    },
+  },
 };
 
 export default nextConfig;
